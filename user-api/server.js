ec94e85d66aab2062cbedf27a5ed33b676ec3833
JulesFEDIT <jules.fedit@edu.ece.fr>
2024-12-30 20:51:26 +0100

    feat: kubernetes environment added

--- user-api/server.js
+++ user-api/server.js
@@ -10,6 +10,9 @@ const PORT = 3000;
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to the User API Application</h1><p>Use the API to create, read, update, and delete users!</p><p>Check the README.md of the project to get more information about how this application.</p>");
+});
 // Use the user routes
 app.use('/api/users', userRoutes);
 
@@ -20,31 +23,9 @@ app.get('/health', (req, res) => {
     });
 });
 
-app.get('/test-redis', async (req, res) => {
-    try {
-      console.log('Testing Redis connection...');
-      const result = await redisClient.ping();
-      console.log('PING response:', result);
-      res.status(200).json({ message: 'Redis is connected', ping: result });
-    } catch (err) {
-      console.error('Redis connection test error:', err.message);
-      console.error('Redis client state:', redisClient.isOpen ? 'Open' : 'Closed');
-      res.status(500).json({ error: 'Redis connection failed', details: err.message });
-    }
-  });
-
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
 
-// Start the server only if this file is executed directly
-// if (require.main === module) {
-//     const PORT = process.env.PORT || 3000;
-//     app.listen(PORT, () => {
-//         console.log(`Server running on http://localhost:${PORT}`);
-//     });
-// }
-
-// Export the app for testing or reusability
 module.exports = app;
